156fa870c56d6f6c1f6abaa69bdee164a7e3f928
Mateescu Denis <118893406+matrex32@users.noreply.github.com>
2024-01-29 13:49:28 +0200

    Adding settings in pom.xml

---
+++ engiconnect/src/main/java/com/engiconnect/EngiconnectApplication.java
@@ -0,0 +1,13 @@
+package com.engiconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngiconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngiconnectApplication.class, args);
+	}
+
+}
---
+++ engiconnect/src/test/java/com/engiconnect/EngiconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engiconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngiconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
