b2caf03257b9c16ad42dc61e4c8f5255af5d08d2
Mateescu Denis <118893406+matrex32@users.noreply.github.com>
2024-02-01 23:43:10 +0200

    Front

---
+++ engiconnect-frontend/src/shared/AppHeader.jsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { AppBar, Toolbar } from "@mui/material"
+import { useState, useContext } from 'react';
+
+function AppHeader() {
+
+    return (
+        <AppBar position="static" style={{backgroundColor:'#4169E1'}}>
+            <Toolbar>
+                
+            </Toolbar>
+
+        </AppBar>
+    )
+}
---
+++ engiconnect-frontend/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = [
+  {
+    entry: './src/login.js',
+    output: {
+      path: path.join(__dirname, '../engiconnect/src/main/resources/static/app'),
+      filename: 'login.js',
+    },
+    mode: "development",
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+            },
+          },
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+  }
+]
