3210259fc13133d11df13559b7c275957dd4a8ce
Mateescu Denis <118893406+matrex32@users.noreply.github.com>
2024-02-02 21:24:49 +0200

    #2 Finishing the RegistrationForm and connecting to the server

--- engiconnect-frontend/src/login/AuthContainer.jsx
+++ engiconnect-frontend/src/login/AuthContainer.jsx
@@ -3,13 +3,25 @@ import React from "react"
 import "../css/GlobalStyle.css"
 import AppHeader from "../shared/AppHeader.jsx"
 import { Grid } from "@mui/material"
+import AuthCard from "./AuthCard.jsx"
 
+/**
+ * Component responsible for user authentication and rendering of both login and registration forms.
+ * @returns {JSX.Element} The JSX representation of the Authentication component.
+ */
 export default function AuthContainer() {
     
     return (
+        /* Main Container */
         <Grid container direction="column">
+            {/* Header Section */}
             <Grid item container>
                 <AppHeader/>
+            </Grid>
+
+              {/* Authentication Forms Section */}
+            <Grid item container>
+                <AuthCard/>
             </Grid>
         </Grid>
     )
